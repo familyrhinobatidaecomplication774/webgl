19314eb6ea0cc3ae7102c64369f13615a5149843
selcukcukur <selcukcukur@outlook.com.tr>
2026-03-29 18:42:28 +0300

    chore(repo): update tsup configuration file

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,6 +6,6 @@ export default defineConfig((options: Options) => ({
   dts         : true,
   minify      : true,
   clean       : true,
-  external    : ["react","vue","nuxt","next","clsx","class-variance-authority"],
+  external    : [],
   ...options
 }))
